ad830798657c1e9102c463717311cb2515181f98
Josh Rutz <joshrutz989@gmail.com>
2012-04-18 20:29:56 -0700

    problem 8 in python, supersexy solution

---
+++ python/p8.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# I think this is the one where you have to find the highest product of any 5 digits in a row in the list
+
+f = file('p8.txt')
+txt = f.read()
+f.close()
+
+txt = txt.strip()
+
+
+length=len(txt)
+
+print max([reduce(lambda x,y: int(x)*int(y), txt[i-5:i]) for i in range(5,length)]) 
+
