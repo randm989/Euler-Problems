019ec0abcc6dcea2dc55769f3007ef8a28c294fe
Josh Rutz <joshrutz989@gmail.com>
2012-05-20 14:48:12 -0700

    p13 in python solution

---
+++ python/p13.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+#Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+
+f = open('p13.txt')
+contents = f.read()
+f.close()
+
+contents = contents.split("\n")
+
+del contents[-1]
+total = 0
+for num in contents :
+	total += int(num)
+
+print str(total)[:10]
