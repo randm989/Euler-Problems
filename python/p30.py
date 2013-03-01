c90e10d0c7cfe27e55e7370281a855835f4a2525
Joshua Rutz <joshrutz989@gmail.com>
2013-03-01 13:46:21 -0800

    Comment changes

--- python/p30.py
+++ python/p30.py
@@ -1,13 +1,22 @@
-
+#Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+#
+#  1634 = 14 + 64 + 34 + 44
+#  8208 = 84 + 24 + 04 + 84
+#  9474 = 94 + 44 + 74 + 44
+#  As 1 = 14 is not a sum it is not included.
+#
+#  The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+#
+#  Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
 
 def sumOfDigitPower(num,power):
 	numStr=str(num)
 	total=0
 	for i in numStr:
-		total+=pow(int(i),power)
+		total+=int(i)**power
 	return total
 
-maxNum=pow(9,5)*5
+maxNum=(9**5)*5
 
 total=0
 for i in range(2,maxNum):
@@ -17,4 +26,4 @@ for i in range(2,maxNum):
 		print(i)
 
 
-print total	
+print "Answer",total	
--- python/p31.py
+++ python/p31.py
@@ -1,4 +1,11 @@
-import math
+#In England the currency is made up of pound L and pence p and there are eight coins in general circulation:
+#
+#  1p, 2p, 5p, 10p, 20p, 50p, L1 (100p) and L2 (200p).
+#  It is possible to make L2 in the following way:
+#
+#    1L1 + 150p + 220p + 15p + 12p + 31p
+#    How many different ways can L2 be made using any number of coins?
+
 
 currencyValues = (200,100,50,20,10,5,2,1)
 
@@ -6,7 +13,7 @@ currencyValues = (200,100,50,20,10,5,2,1)
 def numCurrencyCombinations(values,index=0,currentSum=0):
 	if currentSum >= 200 or index >= len(values):
 		return 0
-	iterations=int((200-currentSum)/math.floor(values[index]))
+	iterations=int((200-currentSum)/int(values[index]))
 	
 	tempSum=currentSum
 	numCombinations = 0
@@ -20,5 +27,5 @@ def numCurrencyCombinations(values,index=0,currentSum=0):
 	return numCombinations
 
 
-print numCurrencyCombinations(currencyValues)
+print "The answer:",numCurrencyCombinations(currencyValues)
 
