ed635029903749e9658b7a9943a55e62afc4448b
Joshua Rutz <joshrutz989@gmail.com>
2013-04-22 14:16:03 -0700

    p19 in c++

--- c++/p19.cpp
+++ c++/p19.cpp
@@ -2,7 +2,45 @@
 
 MAKE_PROTOTYPE(p19)
 
+static int daysInMonth(uint year, uint month)
+{
+  uint days = 31;
+  if ( month == 3 || month == 5 || month == 8 || month == 10 )
+  {
+    days = 30;
+  }
+
+  if ( month == 2 )
+  {
+    days = 28;
+    if ( month % 4 == 0 && !(month % 100 == 0 && month % 400 != 0) )
+    {
+      days = 29;
+    }
+  }
+
+  return days;
+}
+
 int p19::execute() 
 {
-    return 0;
+  uint curYear = 1900;
+  uint curMonth = 0;
+  uint curDayOfWeek = 1;
+  uint numSundays = 0;
+
+  while ( curYear <= 2000 )
+  {
+    for ( curMonth = 0; curMonth < 12; ++curMonth )
+    {
+      if ( curYear > 1900 && curDayOfWeek == 0 )
+      { 
+        numSundays++;
+      }
+      curDayOfWeek += daysInMonth(curYear, curMonth);
+      curDayOfWeek %= 7;
+    }
+    curYear++; 
+  }
+  return numSundays;
 }
